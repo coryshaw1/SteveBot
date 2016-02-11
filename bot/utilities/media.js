15ee70dd5bfaa06eedfda8a75f6c7c796c6bbd2a
coryshaw1 <cory.shaw.dev@gmail.com>
2016-02-11 14:12:48 -0500

    Add fallback if invalid or no Soundcloud API key is set

--- bot/utilities/media.js
+++ bot/utilities/media.js
@@ -19,14 +19,20 @@ module.exports.getLink = function(bot, callback) {
 
         var responseBack = function(error, response, body) {
             if(!error){
-                var body = JSON.parse(body);
-                return callback(body.permalink_url);
+                try{
+                    var json = JSON.parse(body);
+                    return callback(json.permalink_url);
+                }
+                catch(e){
+                    bot.log("error", "BOT", "Soundcloud API error fetching song! Could be caused by invalid Soundcloud API key");
+                    return callback('https://api.dubtrack.fm/song/' + media.id + '/redirect');
+                }
             }
             else {
-                bot.log("info", "BOT", "Soundcloud Error: " + error);
-                return callback('Error... http://google.com');
+                bot.log("error", "BOT", "Soundcloud Error: " + error);
+                return callback('https://api.dubtrack.fm/song/' + media.id + '/redirect');
             }
-        }
+        };
 
         request(options, responseBack);
     }
