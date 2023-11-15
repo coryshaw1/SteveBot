39fd274280cbb915ad44ad075efa2eca82919860
franciscog <557684+FranciscoG@users.noreply.github.com>
2023-11-15 01:33:58 -0500

    more jsdoc and prettier fixes

--- bot/commands/triggers/source.js
+++ bot/commands/triggers/source.js
@@ -1,25 +1,32 @@
-'use strict';
-var repo = require(process.cwd()+'/repo');
-var moment = require('moment');
+"use strict";
+var repo = require(process.cwd() + "/repo");
+const moment = require("moment");
 
+/**
+ *
+ * @param {DubAPI} bot  dubapi instance
+ * @param {import('firebase-admin').database} db   Firebase instance
+ * @param {object} data Room info object
+ * @param {boolean} isBrad
+ */
 module.exports = function (bot, db, data, isBrad = false) {
-  if (data.params === void(0) || data.params.length < 1) {
-    return bot.sendChat('*usage:* !source <trigger_name>');
+  if (data.params === void 0 || data.params.length < 1) {
+    return bot.sendChat("*usage:* !source <trigger_name>");
   }
 
   if (data.params.length > 1) {
-    bot.sendChat('only one trigger at a time');
-    return bot.sendChat('*usage:* !source <trigger_name>');
+    bot.sendChat("only one trigger at a time");
+    return bot.sendChat("*usage:* !source <trigger_name>");
   }
 
   var trigger = data.params[0];
 
-  if (trigger.charAt(0) === '!') {
+  if (trigger.charAt(0) === "!") {
     trigger = trigger.substring(1);
   }
 
-  repo.getTrigger(bot, db, trigger, function(val){
-    if (val !== null){
+  repo.getTrigger(bot, db, trigger, function (val) {
+    if (val !== null) {
       var keys = Object.keys(val);
       var result = val[keys[0]];
       var theAuthor = result.Author || "unknown";
@@ -28,20 +35,20 @@ module.exports = function (bot, db, data, isBrad = false) {
       let chatMsgEnd = ` was created/updated by ${theAuthor}`;
 
       if (isBrad) {
-        if (theAuthor.toLowerCase() === "brad") { 
+        if (theAuthor.toLowerCase() === "brad") {
           return bot.sendChat("Yes");
         }
         return bot.sendChat(`No, it was ${theAuthor}`);
       }
 
       if (result.createdOn && result.createdBy) {
-        let when = moment(result.createdOn).format('MMM Do YYYY');
+        let when = moment(result.createdOn).format("MMM Do YYYY");
         chatMsg += ` was created by ${result.createdBy} on ${when}`;
         extendedInfo = true;
       }
 
-      if (result.lastUpdated && result.status && result.status === 'updated') {
-        let when = moment(result.lastUpdated).format('MMM Do YYYY');
+      if (result.lastUpdated && result.status && result.status === "updated") {
+        let when = moment(result.lastUpdated).format("MMM Do YYYY");
         chatMsg += ` and was last updated by ${theAuthor} on ${when}`;
         extendedInfo = true;
       }
@@ -51,7 +58,6 @@ module.exports = function (bot, db, data, isBrad = false) {
       } else {
         return bot.sendChat(chatMsg + chatMsgEnd);
       }
-
     }
   });
 };
