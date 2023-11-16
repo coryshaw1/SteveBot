a8f181947ab9d81baaa6ee88dd1862ce72927d81
franciscog <557684+FranciscoG@users.noreply.github.com>
2023-11-15 19:38:48 -0500

    fixed missing character

--- bot/commands/fun/fact.js
+++ bot/commands/fun/fact.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * @param {DubAPI} bot
  */
-module.exports = function(bot) {
-  fetch('http://numbersapi.com/random/trivia')
-    .then(res => {
-      if (res.ok) return res.text()
+module.exports = function (bot) {
+  fetch("http://numbersapi.com/random/trivia")
+    .then((res) => {
+      if (res.ok) return res.text();
       else throw new Error(res.status.toString());
     })
-    .then(text => bot.sendChat(text))
-    .catch(err => {
-      bot.log('error', 'BOT', `[!fact] ${err.message}`);
-      bot.sendChat('Bad request to facts...');
-    }
+    .then((text) => bot.sendChat(text))
+    .catch((err) => {
+      bot.log("error", "BOT", `[!fact] ${err.message}`);
+      bot.sendChat("Bad request to facts...");
+    });
 };
