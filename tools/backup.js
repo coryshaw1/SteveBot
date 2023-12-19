5dc332cda882092d8e72f7343436e39bd15fbd26
franciscog <557684+FranciscoG@users.noreply.github.com>
2023-12-18 23:12:40 -0500

    update backup.js

--- tools/backup.js
+++ tools/backup.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 'use strict';
-var fs = require('fs');
-var Database = require('../bot/db.js');
-const _private = require('../private/prod/settings.js');
-var settings = _private.settings;
-var svcAcct = _private.svcAcct;
-var BASEURL = settings.FIREBASE.BASEURL;
-var db = new Database(svcAcct, BASEURL);
+const fs = require('fs');
+const Database = require('../bot/db.js');
+const settings = require('../private/prod/settings.js');
+const svcAcct = require('../private/prod/serviceAccountCredentials.json')
+
+const BASEURL = settings.FIREBASE.BASEURL;
+const db = new Database(svcAcct, BASEURL);
 
 /*************************************
   Back up our DB via Cron.  Mainly
@@ -18,7 +18,7 @@ db.ref().once('value', function(snapshot) {
   var val = snapshot.val();
   if (val !== null) {
     // save backups outside of the repo
-    var loc = '../../Derpy-backups';
+    var loc = '../../backups';
     fs.writeFileSync(`${loc}/backup-${Date.now()}.json`, JSON.stringify(val), 'utf8');
     process.exit();
   }
